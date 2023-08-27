217288141ee4ddf3b8db3726e7d297368da86cb9
dungnguyen2402 <117297846+dungnguyen2402@users.noreply.github.com>
2023-08-27 16:06:23 +0700

    FE|feat :giao dien tang dang ki - dang nhap (#6)
    Co-authored-by: Trường Nguyễn <97250708+truongnq0509@users.noreply.github.com>

--- client/src/layouts/AuthLayout.tsx
+++ client/src/layouts/AuthLayout.tsx
@@ -1,15 +1,41 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 
 type Props = {};
 
 const AuthLayout = (props: Props) => {
 	return (
-		<Box>
-			<h1>Layout Auth</h1>
-			<Outlet />
-		</Box>
+		<Grid
+			templateColumns="repeat(12, 1fr)"
+			h="100vh"
+		>
+			<GridItem
+				w="100%"
+				h="full"
+				colSpan={{
+					sm: 12,
+					md: 12,
+					lg: 12,
+					xl: 5,
+					"2xl": 5,
+				}}
+			>
+				<Outlet />
+			</GridItem>
+			<GridItem
+				w="100%"
+				h="full"
+				colSpan={{
+					sm: 0,
+					md: 0,
+					lg: 0,
+					xl: 7,
+					"2xl": 7,
+				}}
+				bgColor="bg.gray"
+			></GridItem>
+		</Grid>
 	);
 };
 
