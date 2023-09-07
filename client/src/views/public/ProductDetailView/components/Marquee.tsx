2a4a3133f4ba71b329f9903254b843ab1bdb5ed3
TruongNguyen <truongmoba157@gmail.com>
2023-09-07 21:58:22 +0700

    FE: ...

--- client/src/views/public/ProductDetailView/components/Marquee.tsx
+++ client/src/views/public/ProductDetailView/components/Marquee.tsx
@@ -20,35 +20,60 @@ const dataFake = [
 	{
 		id: 1,
 		title: "Tận tâm tư vấn",
-		icon: <BellIcon size={5} color="black" />,
+		icon: (
+			<BellIcon
+				size={5}
+				color="black"
+			/>
+		),
 		description:
 			"Giúp khách hàng lựa chọn sản phẩm đúng nhu cầu là trách nhiệm đầu tiên của Nhân viên tư vấn tại ThinkPro.",
 	},
 	{
 		id: 1,
 		title: "Tận tâm tư vấn",
-		icon: <BellIcon size={5} color="black" />,
+		icon: (
+			<BellIcon
+				size={5}
+				color="black"
+			/>
+		),
 		description:
 			"Giúp khách hàng lựa chọn sản phẩm đúng nhu cầu là trách nhiệm đầu tiên của Nhân viên tư vấn tại ThinkPro.",
 	},
 	{
 		id: 1,
 		title: "Tận tâm tư vấn",
-		icon: <BellIcon size={5} color="black" />,
+		icon: (
+			<BellIcon
+				size={5}
+				color="black"
+			/>
+		),
 		description:
 			"Giúp khách hàng lựa chọn sản phẩm đúng nhu cầu là trách nhiệm đầu tiên của Nhân viên tư vấn tại ThinkPro.",
 	},
 	{
 		id: 1,
 		title: "Tận tâm tư vấn",
-		icon: <BellIcon size={5} color="black" />,
+		icon: (
+			<BellIcon
+				size={5}
+				color="black"
+			/>
+		),
 		description:
 			"Giúp khách hàng lựa chọn sản phẩm đúng nhu cầu là trách nhiệm đầu tiên của Nhân viên tư vấn tại ThinkPro.",
 	},
 	{
 		id: 1,
 		title: "Tận tâm tư vấn",
-		icon: <BellIcon size={5} color="black" />,
+		icon: (
+			<BellIcon
+				size={5}
+				color="black"
+			/>
+		),
 		description:
 			"Giúp khách hàng lựa chọn sản phẩm đúng nhu cầu là trách nhiệm đầu tiên của Nhân viên tư vấn tại ThinkPro.",
 	},
@@ -59,11 +84,25 @@ const MarqueeReact = (props: Props) => {
 	const btnRef = useRef(null);
 	return (
 		<>
-			<Flex borderRadius={"3px"} bg={"#F5FDFF"} as={"button"} onClick={onOpen} mt={"3"}>
+			<Flex
+				borderRadius={"3px"}
+				bg={"#F5FDFF"}
+				as={"button"}
+				onClick={onOpen}
+				mt={"3"}
+			>
 				<Marquee>
 					{dataFake.map((data) => (
-						<Flex alignItems={"center"} fontSize={"12px"} mx={"2"}>
-							<Box bg={"blue.400"} borderRadius={"full"} p={"1"}>
+						<Flex
+							alignItems={"center"}
+							fontSize={"12px"}
+							mx={"2"}
+						>
+							<Box
+								bg={"blue.400"}
+								borderRadius={"full"}
+								p={"1"}
+							>
 								{data.icon}
 							</Box>
 							{data.title}
@@ -83,27 +122,57 @@ const MarqueeReact = (props: Props) => {
 					justifyContent="center"
 					className="btn-next"
 				>
-					<NavArrowRightIcon size={4} strokeWidth={3} color="text.black" />
+					<NavArrowRightIcon
+						size={4}
+						strokeWidth={3}
+						color="text.black"
+					/>
 				</Flex>
 			</Flex>
-			<Modal isOpen={isOpen} finalFocusRef={btnRef} onClose={onClose} size={"xl"} scrollBehavior={scrollBehavior}>
+			<Modal
+				isOpen={isOpen}
+				finalFocusRef={btnRef}
+				onClose={onClose}
+				size={"xl"}
+				scrollBehavior={scrollBehavior}
+			>
 				<ModalOverlay />
 				<ModalContent>
 					<ModalHeader fontSize={"20px"}>Tự tin mua sắm cùng ThinkPro</ModalHeader>
 					<ModalCloseButton />
 					<ModalBody mx={"2"}>
-						<Box bg={"#0A2545"} p={"14px"} borderRadius={"3px"}>
-							<Text as={"p"} color={"white"} fontSize={"10px"}>
+						<Box
+							bg={"#0A2545"}
+							p={"14px"}
+							borderRadius={"3px"}
+						>
+							<Text
+								as={"p"}
+								color={"white"}
+								fontSize={"10px"}
+							>
 								Một thành viên của
 							</Text>
-							<Text as={"p"} color={"blue.300"} fontSize={"16px"} fontWeight={600}>
+							<Text
+								as={"p"}
+								color={"blue.300"}
+								fontSize={"16px"}
+								fontWeight={600}
+							>
 								ONWARD TOGETHER GROUP
 							</Text>
-							<Text as={"p"} color={"white"} fontSize={"10px"}>
+							<Text
+								as={"p"}
+								color={"white"}
+								fontSize={"10px"}
+							>
 								Tập đoàn bán lẻ Công nghệ - Nội thất phục vụ khách hàng tốt nhất.
 							</Text>
 						</Box>
-						<Box bg={"#FDF5FF"} borderRadius={"3px"}>
+						<Box
+							bg={"#FDF5FF"}
+							borderRadius={"3px"}
+						>
 							{/* Item */}
 							{dataFake.map((data: any) => (
 								<Flex
@@ -117,7 +186,10 @@ const MarqueeReact = (props: Props) => {
 								>
 									<Box w={"5%"}>{data.icon}</Box>
 									<Box w={"80%"}>
-										<Text fontSize={"14px"} fontWeight={600}>
+										<Text
+											fontSize={"14px"}
+											fontWeight={600}
+										>
 											{data.title}
 										</Text>
 										<Text fontSize={"12px"}>{data.description}</Text>
@@ -127,7 +199,11 @@ const MarqueeReact = (props: Props) => {
 						</Box>
 					</ModalBody>
 					<ModalFooter>
-						<Button onClick={onClose} bg={"#0A2540"} px={"20"}>
+						<Button
+							onClick={onClose}
+							bg={"#0A2540"}
+							px={"20"}
+						>
 							Đóng
 						</Button>
 					</ModalFooter>
