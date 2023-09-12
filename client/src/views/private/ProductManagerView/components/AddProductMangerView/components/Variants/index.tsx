ff9614c3aaa39d1eb5da384f30851a728eeb6b25
TruongNguyen <truongmoba157@gmail.com>
2023-09-12 22:29:03 +0700

    FE/feat: quản lý sản phẩm

--- client/src/views/private/ProductManagerView/components/AddProductMangerView/components/Variants/index.tsx
+++ client/src/views/private/ProductManagerView/components/AddProductMangerView/components/Variants/index.tsx
@@ -4,25 +4,7 @@ import React from "react";
 type Props = {};
 
 const Variants = (props: Props) => {
-	return (
-		<Box
-			bgColor="bg.white"
-			px="8"
-			py="6"
-			rounded="xl"
-		>
-			<Heading
-				fontSize="lg"
-				fontWeight="semibold"
-			>
-				Variants
-			</Heading>
-			<Divider
-				mt="3"
-				mb="4"
-			/>
-		</Box>
-	);
+	return <Box>Variants</Box>;
 };
 
 export default Variants;
