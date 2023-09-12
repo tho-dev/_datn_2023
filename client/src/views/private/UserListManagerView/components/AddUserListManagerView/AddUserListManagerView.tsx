f6edff9b4178737aa08b51a7d719e7a385ea8a8b
thuannd <thuansmile2479@gmail.com>
2023-09-13 04:18:04 +0700

    Add user

---
+++ client/src/views/private/UserListManagerView/components/AddUserListManagerView/AddUserListManagerView.tsx
@@ -0,0 +1,59 @@
+import { Tabs, TabList, TabPanels, Tab, TabPanel, Text, Box, Spacer } from '@chakra-ui/react'
+import Profine from './conponents/Profine'
+import Address from './conponents/Address'
+import Confirmation from './conponents/Confirmation'
+
+type Props = {};
+
+const AddUserListManagerView = (props: Props) => { 
+
+
+  return (
+    <Tabs variant='unstyled'>
+      <TabList w={"70%"} m={"10px auto"}>
+        <Box borderRadius={"100%"} h={"44px"} borderColor='blue.500' border={"2px"}>
+          <Tab _selected={{ color: 'white', bg: 'blue.500', borderRadius: "100%" }}>1</Tab>
+        </Box>
+        <Text m={2} fontWeight={700} fontSize={"20px"}>Profine</Text>
+        <Spacer />
+        <Box borderRadius={"100%"} h={"44px"} border={"2px"}>
+          <Tab _selected={{ color: 'white', bg: 'blue.500', borderRadius: "100%" }}>2</Tab>
+        </Box>
+        <Text m={2} fontWeight={700} fontSize={"20px"}>Billing address</Text>
+        <Spacer />
+        <Box borderRadius={"100%"} h={"44px"} border={"2px"}>
+          <Tab _selected={{ color: 'white', bg: 'blue.500', borderRadius: "100%" }}>3</Tab>
+        </Box>
+        <Text m={2} fontWeight={700} fontSize={"20px"}>Confirmation</Text>
+      </TabList>
+      <TabPanels
+        my={10}
+        bg="white"
+        w={"100%"}
+        borderRadius="2xl"
+        py={{
+          sm: "6",
+          lg: "9",
+        }}
+        px={{
+          sm: "10",
+          md: "10",
+          lg: "28",
+          xl: "28",
+        }}
+      >
+        <TabPanel>
+          <Profine />
+        </TabPanel>
+        <TabPanel>
+          <Address />
+        </TabPanel>
+        <TabPanel>
+          <Confirmation />
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
+  )
+}
+
+export default AddUserListManagerView
