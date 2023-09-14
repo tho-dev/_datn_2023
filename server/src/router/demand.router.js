44e47835c0f4ce7dcf72dc69a0896a93c03aedbd
DungNguyen2402 <117297846+DungNguyen2402@users.noreply.github.com>
2023-09-14 23:05:15 +0700

    BE|feat: CRUD Demand

--- server/src/router/demand.router.js
+++ server/src/router/demand.router.js
@@ -1,9 +1,14 @@
 import express from "express"
-import { getAllDemand } from "../controllers/demand.controller"
+import { getAllDemand, getOneDemand, updateDemand, createDemand, removeDemand } from "../controllers/demand.controller"
 
 
 const router = express.Router()
 
 router.get('/', getAllDemand)
+router.get('/:slug', getOneDemand)
+router.post('/', createDemand)
+router.put('/:id', updateDemand)
+router.delete('/:id', removeDemand)
+
 
 export default router
