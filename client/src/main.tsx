796673dba265c4ad6ef45999166a0f92f627e24f
Ha Van Hiep <chuongduong0123@gmail.com>
2023-09-17 23:08:48 +0700

    update redux

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "flatpickr/dist/themes/material_green.css";
 import "./index.css";
-// react
+// redux-toolkit
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from "./redux/store.ts";
+import { Provider } from "react-redux";
 
 // style font
 import "@fontsource/quicksand/300.css";
@@ -15,36 +18,17 @@ import "@fontsource/quicksand/700.css";
 // custom them chakra ui
 import customTheme from "~/theme";
 import { ChakraProvider, CSSReset } from "@chakra-ui/react";
+
 // cấu hình client state
-import { persistQueryClient } from "@tanstack/react-query-persist-client";
-import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      cacheTime: 1000 * 60 * 60 * 24, // 24 hours
-    },
-  },
-});
-
-const localStoragePersister = createSyncStoragePersister({
-  storage: window.localStorage,
-  key: "users",
-});
-
-persistQueryClient({
-  queryClient,
-  persister: localStoragePersister,
-});
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={customTheme} resetCSS={true}>
       <CSSReset />
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
     </ChakraProvider>
   </React.StrictMode>
 );
