96ab208610b36c58d316d9181ab5aa73fc6bb7d8
buiquynh-123 <quynhbtdph19396@gmail.com>
2023-09-19 08:24:14 +0700

    create dashboard admin page ui & api post

---
+++ server/src/models/post.model.js
@@ -0,0 +1,65 @@
+import { Schema, model } from 'mongoose';
+import slug from 'mongoose-slug-updater';
+import mongoosePaginate from 'mongoose-paginate-v2';
+import mongooseDelete from 'mongoose-delete';
+
+const plugins = [slug, mongoosePaginate, mongooseDelete];
+const brandSchema = new Schema(
+  {
+    title: {
+      type: String,
+      default: true,
+    },
+    content: {
+      type: String,
+      default: true,
+    },
+    author: {
+      type: String,
+      default: true,
+    },
+    public_date: {
+      type: Date,
+      default: true,
+    },
+    thumbnail: {
+      id: String,
+      url: String,
+    },
+    views: {
+      type: Number,
+    },
+    likes: {
+      type: Number,
+    },
+    comments: {
+      type: String,
+    },
+    publication_status: {
+      type: Boolean,
+    },
+    created_at: {
+      type: Date,
+      default: Date.now,
+    },
+    updated_at: {
+      type: Date,
+      default: Date.now,
+    },
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+    delete_at: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    collection: 'posts',
+    timestamps: false,
+    versionKey: false,
+  }
+);
+plugins.forEach((item) => brandSchema.plugin(item, { overrideMethods: true }));
+export default model('Post', brandSchema);
--- server/src/validations/index.js
+++ server/src/validations/index.js
@@ -1,4 +1,4 @@
-import joi from "joi";
+import joi from 'joi';
 
 export const productSchema = joi.object({
   name: joi.string().required(),
@@ -17,17 +17,16 @@ export const productSchema = joi.object({
   deletedAt: joi.date().default(null),
   deleted: joi.boolean().default(false),
 });
-
-export const userSchema = joi.object({
-  first_name: joi.string().required(),
-  last_name: joi.string().required(),
-  email: joi.string().required().email(),
-  password: joi.string().required().min(6),
-  confirm_password: joi.string().required().valid(joi.ref("password")),
-  phone: joi.number().required(),
-  avatar: joi.string(),
-  location: joi.string(),
-  role: joi.string().default("customer"),
+export const postSchema = joi.object({
+  title: joi.string().required(),
+  content: joi.string().required(),
+  author: joi.string().required(),
+  public_date: joi.string().required(),
+  thumbnail: joi.string().required(),
+  views: joi.string().required(),
+  likes: joi.string().required(),
+  comments: joi.string().required(),
+  publication_status: joi.string().required(),
   createdAt: joi.string().default(() => new Date()),
   updatedAt: joi.string().default(() => new Date()),
   deletedAt: joi.date().default(null),
