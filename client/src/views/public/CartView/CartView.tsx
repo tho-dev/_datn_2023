7adfff5a3b06cd9c60381a9675866806ad46b006
Ha Van Hiep <chuongduong0123@gmail.com>
2023-10-01 08:09:30 +0700

    update 01-10

--- client/src/views/public/CartView/CartView.tsx
+++ client/src/views/public/CartView/CartView.tsx
@@ -4,49 +4,51 @@ import { Helmet } from "react-helmet";
 import { HelmetProvider } from "react-helmet-async";
 import ItemCart from "./components/ItemCart";
 import OrderSummary from "./components/OrderSummary";
+import { useNavigate } from "react-router";
 
 type Props = {};
 
 const CartView = (props: Props) => {
-	return (
-		<HelmetProvider>
-			<Helmet>
-				<title>ThinkPro | Giỏ hàng của bạn</title>
-			</Helmet>
-			<Heading
-				pt={"4"}
-				fontSize={"20px"}
-			>
-				Giỏ hàng (0)
-			</Heading>
-			<Box
-				display="flex"
-				flexDirection={{ base: "column", md: "row" }}
-				my={"5"}
-				w={"full"}
-			>
-				<Box
-					backgroundColor={"white"}
-					borderRadius={"md"}
-					py={"5"}
-					mr={"5"}
-					w={{ md: "80%", base: "full" }}
-				>
-					<ItemCart />
-				</Box>
-				<Box
-					backgroundColor={"white"}
-					borderRadius={"md"}
-					py={"5"}
-					px={"5"}
-					w={{ md: "40%", base: "full" }}
-					h={"full"}
-				>
-					<OrderSummary />
-				</Box>
-			</Box>
-		</HelmetProvider>
-	);
+  const navigate = useNavigate();
+  const handlePayment = () => {
+    navigate("/thanh-toan");
+  };
+  return (
+    <HelmetProvider>
+      <Helmet>
+        <title>ThinkPro | Giỏ hàng của bạn</title>
+      </Helmet>
+      <Heading pt={"4"} fontSize={"20px"}>
+        Giỏ hàng (0)
+      </Heading>
+      <Box
+        display="flex"
+        flexDirection={{ base: "column", md: "row" }}
+        my={"5"}
+        w={"full"}
+      >
+        <Box
+          backgroundColor={"white"}
+          borderRadius={"md"}
+          py={"5"}
+          mr={"5"}
+          w={{ md: "80%", base: "full" }}
+        >
+          <ItemCart />
+        </Box>
+        <Box
+          backgroundColor={"white"}
+          borderRadius={"md"}
+          py={"5"}
+          px={"5"}
+          w={{ md: "40%", base: "full" }}
+          h={"full"}
+        >
+          <OrderSummary handlePayment={handlePayment} />
+        </Box>
+      </Box>
+    </HelmetProvider>
+  );
 };
 
 export default CartView;
