9557df5ac16906b43d1e7e12a110b5c3b791070d
thuannd <thuansmile2479@gmail.com>
2023-10-05 22:28:29 +0700

    [FE] Fix viewed products

--- client/src/redux/slices/globalSlice.ts
+++ client/src/redux/slices/globalSlice.ts
@@ -1,32 +1,38 @@
-import { createSlice } from "@reduxjs/toolkit";
-// import { IUser } from "~/interface/auth";
-
-
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export interface IGlobalState {
-    isLogin: boolean;
-    user: any;
+  isLogin: boolean;
+  user: any;
+  accessToken: string;
+  viewedItems: any[];
 }
 
 const initialState: IGlobalState = {
-    isLogin: false,
-    user: {} as any
-}
+  isLogin: false,
+  user: {} as any,
+  accessToken: "",
+  viewedItems: [], 
+};
 
 const globalSlice = createSlice({
-    name: "global",
-    initialState,
-    reducers: {
-        login: (state, action) => {
-            state.user = action.payload;
-            state.isLogin = true;
-        },
-        logout: (state, action) => {
-            state.user = {}
-            state.isLogin = action.payload;
-        },
+  name: "global",
+  initialState,
+  reducers: {
+    login: (state, action: PayloadAction<{ data: any; accessToken: string }>) => {
+      state.user = action.payload.data;
+      state.isLogin = true;
+      state.accessToken = action.payload.accessToken;
     },
+    logout: (state) => {
+      state.user = {};
+      state.isLogin = false;
+      state.accessToken = "";
+    },
+    addViewedItem: (state, action: PayloadAction<any>) => {
+      state.viewedItems = [action.payload, ...state.viewedItems];
+    },
+  },
 });
 
-export const { login, logout } = globalSlice.actions;
+export const { login, logout, addViewedItem } = globalSlice.actions;
 export default globalSlice.reducer;
