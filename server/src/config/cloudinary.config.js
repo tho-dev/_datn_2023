2e73bbe9abd27f156cffba335f6e2b7ca7f04287
TruongNguyen <truongmoba157@gmail.com>
2023-10-10 16:25:46 +0700

    BE|feat: crop image in cloudinary

--- server/src/config/cloudinary.config.js
+++ server/src/config/cloudinary.config.js
@@ -3,9 +3,9 @@ import { CloudinaryStorage } from "multer-storage-cloudinary";
 import multer from "multer";
 
 cloudinary.config({
-  cloud_name: "dbhyz5cgb",
-  api_key: "736468232726189",
-  api_secret: "4gaPgk7wF-l4aC5MPvDUX3Px8WI",
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
 });
 
 const storage = new CloudinaryStorage({
@@ -14,10 +14,13 @@ const storage = new CloudinaryStorage({
     let folder = "products";
     let width = 1000;
     let height = 1000;
+    let url = ''
+    let opacity = 100
 
     if (file.fieldname == "assets") {
       width = 1000;
       height = 1000;
+      url = 'https://res.cloudinary.com/dctvtsnuk/image/upload/v1696929234/kbgvy5dqhbmjbghoba01.svg'
     } else if (file.fieldname == "brand") {
       width = 120;
       height = 120;
@@ -29,13 +32,22 @@ const storage = new CloudinaryStorage({
     } else if (file.fieldname == "thumbnail") {
       width = 460;
       height = 460;
+      url = 'https://res.cloudinary.com/dctvtsnuk/image/upload/v1696929020/dlery9gg8ccqftph9hpn.svg'
+      opacity = 70
     }
+    // #27bece
+
+    const polytech = url ? [{ opacity: opacity, overlay: { url: url, crop: "fit" } },
+    { flags: "layer_apply" }] : []
 
     // crop cắt theo tỉ lệ theo kích thước nó giống object-fit...
     return {
       folder: `thinkpro/${folder}`,
       allowedFormats: ["jpg", "png"],
-      transformation: [{ width, height, crop: "fit" }],
+      transformation: [
+        { width, height, crop: "fit" },
+        ...polytech
+      ],
     };
   },
 });
