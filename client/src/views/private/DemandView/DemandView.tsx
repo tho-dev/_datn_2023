74da55f8c45a2d8636dee80e8b52664730eff9e8
TruongNguyen <truongmoba157@gmail.com>
2023-10-10 16:52:38 +0700

    FE|featL fix bug trong demand

--- client/src/views/private/DemandView/DemandView.tsx
+++ client/src/views/private/DemandView/DemandView.tsx
@@ -1,17 +1,17 @@
 import { Link as ReactRouterLink } from "react-router-dom";
 import { Box, Flex, Heading, Text } from "@chakra-ui/layout";
 import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    Button,
-    Input,
-    Menu,
-    MenuButton,
-    MenuItem,
-    MenuList,
-    useDisclosure,
-    useToast,
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	Button,
+	Input,
+	Menu,
+	MenuButton,
+	MenuItem,
+	MenuList,
+	useDisclosure,
+	useToast,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { SearchIcon, PlusCircleIcon, TraskIcon, EditIcon } from "~/components/common/Icons";
@@ -23,76 +23,67 @@ import { createColumnHelper } from "@tanstack/react-table";
 import TableThinkPro from "~/components/TableThinkPro";
 import ConfirmThinkPro from "~/components/ConfirmThinkPro";
 
-
-
-
 type Props = {};
 
 const DemandView = (props: Props) => {
-    const toast = useToast();
+	const toast = useToast();
 
-    const [id, setId] = useState(null);
-    const [demand, setDemand] = useState<any>(null);
-    const [parents, setParents] = useState<any>([]);
-    const columnHelper = createColumnHelper<any>();
-    const {
-        isOpen: isOpenActionCreateDemand,
-        onOpen: onOpenActionCreateDemand,
-        onClose: onCloseActionCreateDemand,
-    } = useDisclosure();
-    const {
-        isOpen: isOpenActionUpdateDemand,
-        onOpen: onOpenActionUpdateDemand,
-        onClose: onCloseActionUpdateDemand,
-    } = useDisclosure();
-    const { isOpen: isOpenComfirm, onOpen: onOpenConfirm, onClose: onCloseComfirm } = useDisclosure();
+	const [id, setId] = useState(null);
+	const [demand, setDemand] = useState<any>(null);
+	const [parents, setParents] = useState<any>([]);
+	const columnHelper = createColumnHelper<any>();
+	const {
+		isOpen: isOpenActionCreateDemand,
+		onOpen: onOpenActionCreateDemand,
+		onClose: onCloseActionCreateDemand,
+	} = useDisclosure();
+	const {
+		isOpen: isOpenActionUpdateDemand,
+		onOpen: onOpenActionUpdateDemand,
+		onClose: onCloseActionUpdateDemand,
+	} = useDisclosure();
+	const { isOpen: isOpenComfirm, onOpen: onOpenConfirm, onClose: onCloseComfirm } = useDisclosure();
 
-    const [deleteDemand] = useDeleteDemandMutation();
-    const { data: demands, isLoading } = useGetAllDemandQuery({
-        _parent: true,
-    });
-    // console.log(demands);
+	const [deleteDemand] = useDeleteDemandMutation();
+	const { data: demands, isLoading } = useGetAllDemandQuery({});
 
+	useEffect(() => {
+		if (demands) {
+			const parentsFilter = demands?.data?.map((demand: any) => {
+				return {
+					label: demand?.name,
+					value: demand?._id,
+				};
+			});
 
+			setParents(parentsFilter);
+		}
+	}, [demands, isLoading]);
 
-    useEffect(() => {
-        if (demands) {
-            const parentsFilter = demands?.data?.items?.map((demand: any) => {
-                return {
-                    label: demand?.name,
-                    value: demand?._id,
-                };
-            });
+	const handleDeleteDemand = async () => {
+		try {
+			await deleteDemand(id as any).unwrap();
+			toast({
+				title: "Thành công",
+				duration: 1600,
+				position: "top-right",
+				status: "success",
+				description: "Xóa nhu cầu thành công",
+			});
+		} catch (error: any) {
+			toast({
+				title: "Có lỗi",
+				duration: 1600,
+				position: "top-right",
+				status: "error",
+				description: JSON.stringify(error?.data?.errors),
+			});
+		}
 
-            setParents(parentsFilter);
-        }
-    }, [demands, isLoading]);
+		onCloseComfirm();
+	};
 
-
-    const handleDeleteDemand = async () => {
-        try {
-            await deleteDemand(id as any).unwrap();
-            toast({
-                title: "Thành công",
-                duration: 1600,
-                position: "top-right",
-                status: "success",
-                description: "Xóa danh mục thành công",
-            });
-        } catch (error: any) {
-            toast({
-                title: "Có lỗi",
-                duration: 1600,
-                position: "top-right",
-                status: "error",
-                description: JSON.stringify(error?.data?.errors),
-            });
-        }
-
-        onCloseComfirm();
-    };
-
-    const columns = [
+	const columns = [
 		columnHelper.accessor("#", {
 			cell: (info) => {
 				const index = info.row.index;
@@ -110,7 +101,7 @@ const DemandView = (props: Props) => {
 			cell: (info) => `/${info.getValue()}`,
 			header: "Đường dẫn",
 		}),
-		
+
 		columnHelper.accessor("action", {
 			cell: ({ row }) => {
 				const doc = row?.original;
@@ -167,139 +158,136 @@ const DemandView = (props: Props) => {
 			header: "Action",
 		}),
 	];
-    return (
-        <>
-            <Box
-                bgColor="bg.white"
-                px="6"
-                py="8"
-                mb="8"
-                rounded="lg"
-            >
-                <Flex
-                    alignItems="center"
-                    justifyContent="space-between"
-                    pb="5"
-                >
-                    <Heading
-                        as="h2"
-                        fontSize="18"
-                    >
-                        Quản lý nhu cầu
-                    </Heading>
-                    <Box>
-                        <Breadcrumb
-                            spacing="8px"
-                            separator="/"
-                            fontSize="sm"
-                        >
-                            <BreadcrumbItem>
-                                <BreadcrumbLink
-                                    as={ReactRouterLink}
-                                    to="/admin"
-                                >
-                                    Home
-                                </BreadcrumbLink>
-                            </BreadcrumbItem>
+	return (
+		<>
+			<Box
+				bgColor="bg.white"
+				px="6"
+				py="8"
+				mb="8"
+				rounded="lg"
+			>
+				<Flex
+					alignItems="center"
+					justifyContent="space-between"
+					pb="5"
+				>
+					<Heading
+						as="h2"
+						fontSize="18"
+					>
+						Quản lý nhu cầu
+					</Heading>
+					<Box>
+						<Breadcrumb
+							spacing="8px"
+							separator="/"
+							fontSize="sm"
+						>
+							<BreadcrumbItem>
+								<BreadcrumbLink
+									as={ReactRouterLink}
+									to="/admin"
+								>
+									Home
+								</BreadcrumbLink>
+							</BreadcrumbItem>
 
-                            <BreadcrumbItem isCurrentPage>
-                                <BreadcrumbLink href="nhu-cau">Nhu cầu</BreadcrumbLink>
-                            </BreadcrumbItem>
-                        </Breadcrumb>
-                    </Box>
-                </Flex>
-                <Flex
-                    alignItems="center"
-                    justifyContent="space-between"
-                    mb="6"
-                >
-                    <Flex
-                        px="4"
-                        rounded="4px"
-                        alignItems="center"
-                        borderWidth="1px"
-                        borderColor="#e9ebec"
-                    >
-                        <Flex
-                            as="span"
-                            alignItems="center"
-                            justifyContent="center"
-                        >
-                            <SearchIcon
-                                size={5}
-                                color="text.black"
-                                strokeWidth={1}
-                            />
-                        </Flex>
-                        <Input
-                            border="none"
-                            padding="0.6rem 0.9rem"
-                            fontSize="15"
-                            fontWeight="medium"
-                            lineHeight="1.5"
-                            w="260px"
-                            placeholder="Nhu cầu..."
-                        />
-                    </Flex>
-                    <Button
-                        leftIcon={
-                            <PlusCircleIcon
-                                size={5}
-                                color="text.white"
-                            />
-                        }
-                        px="4"
-                        lineHeight="2"
-                        bgColor="bg.green"
-                        onClick={onOpenActionCreateDemand}
-                    >
-                        Tạo Mới
-                    </Button>
-                </Flex>
+							<BreadcrumbItem isCurrentPage>
+								<BreadcrumbLink href="nhu-cau">Nhu cầu</BreadcrumbLink>
+							</BreadcrumbItem>
+						</Breadcrumb>
+					</Box>
+				</Flex>
+				<Flex
+					alignItems="center"
+					justifyContent="space-between"
+					mb="6"
+				>
+					<Flex
+						px="4"
+						rounded="4px"
+						alignItems="center"
+						borderWidth="1px"
+						borderColor="#e9ebec"
+					>
+						<Flex
+							as="span"
+							alignItems="center"
+							justifyContent="center"
+						>
+							<SearchIcon
+								size={5}
+								color="text.black"
+								strokeWidth={1}
+							/>
+						</Flex>
+						<Input
+							border="none"
+							padding="0.6rem 0.9rem"
+							fontSize="15"
+							fontWeight="medium"
+							lineHeight="1.5"
+							w="260px"
+							placeholder="Nhu cầu..."
+						/>
+					</Flex>
+					<Button
+						leftIcon={
+							<PlusCircleIcon
+								size={5}
+								color="text.white"
+							/>
+						}
+						px="4"
+						lineHeight="2"
+						bgColor="bg.green"
+						onClick={onOpenActionCreateDemand}
+					>
+						Tạo Mới
+					</Button>
+				</Flex>
 
-                {/* Danh sách */}
-                <TableThinkPro
+				{/* Danh sách */}
+				<TableThinkPro
 					columns={columns}
-					data={demands?.data?.items || []}
+					data={demands?.data || []}
 				/>
-                
 
-
-                {/* Cofirm */}
-                <ConfirmThinkPro
-                    isOpen={isOpenComfirm}
-                    onClose={onCloseComfirm}
-                    content="Bạn có muốn xóa bỏ thương hiệu này không?"
-                    handleClick={handleDeleteDemand}
-                />
-            </Box>
-            {/* Form */}
-            <DialogThinkPro
-                isOpen={isOpenActionCreateDemand}
-                onClose={onCloseActionCreateDemand}
-                isCentered
-                title={<Heading fontSize="18">Tạo mới thương hiệu</Heading>}
-            >
-                <ActionCreateDemand
-                    onClose={onCloseActionCreateDemand}
-                    parents={parents}
-                />
-            </DialogThinkPro>
-            <DialogThinkPro
-                isOpen={isOpenActionUpdateDemand}
-                onClose={onCloseActionUpdateDemand}
-                isCentered
-                title={<Heading fontSize="18">Cập nhật thương hiệu</Heading>}
-            >
-                <ActionUpdateDemand
-                    onClose={onCloseActionUpdateDemand}
-                    demand={demand}
-                    parents={parents}
-                />
-            </DialogThinkPro>
-        </>
-    );
+				{/* Cofirm */}
+				<ConfirmThinkPro
+					isOpen={isOpenComfirm}
+					onClose={onCloseComfirm}
+					content="Bạn có muốn xóa bỏ nhu cầu này không?"
+					handleClick={handleDeleteDemand}
+				/>
+			</Box>
+			{/* Form */}
+			<DialogThinkPro
+				isOpen={isOpenActionCreateDemand}
+				onClose={onCloseActionCreateDemand}
+				isCentered
+				title={<Heading fontSize="18">Tạo mới nhu cầu</Heading>}
+			>
+				<ActionCreateDemand
+					onClose={onCloseActionCreateDemand}
+					parents={parents}
+				/>
+			</DialogThinkPro>
+			<DialogThinkPro
+				isOpen={isOpenActionUpdateDemand}
+				onClose={onCloseActionUpdateDemand}
+				isCentered
+				title={<Heading fontSize="18">Cập nhật nhu cầu</Heading>}
+			>
+				<ActionUpdateDemand
+					onClose={onCloseActionUpdateDemand}
+					demand={demand}
+					parents={parents}
+				/>
+			</DialogThinkPro>
+		</>
+	);
 };
 
 export default DemandView;
-
