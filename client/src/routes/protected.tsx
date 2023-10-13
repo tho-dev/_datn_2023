515684686e63edc60fbee09aecd437394a480b10
Ha Van Hiep <chuongduong0123@gmail.com>
2023-10-13 09:57:44 +0700

    FE|feat update

--- client/src/routes/protected.tsx
+++ client/src/routes/protected.tsx
@@ -1,16 +1,23 @@
+import { Route, Routes, Navigate, Outlet } from "react-router-dom";
 export const protectedRouter = () => {
-	const user = JSON.parse(localStorage.getItem("persist:root") as string);
-	if (user) {
-		const auth = JSON.parse(user?.global);
+  const user = JSON.parse(localStorage.getItem("persist:root") as string);
+  if (user) {
+    const auth = JSON.parse(user?.global);
 
-		if (auth?.isLogin == true) {
-			if (auth?.user?.role == "admin") {
-				return true;
-			}
-			return false;
-		} else {
-			return false;
-		}
-	}
-	return false;
+    if (auth?.isLogin == true) {
+      if (auth?.user?.role == "admin") {
+        return true;
+      }
+      return false;
+    } else {
+      return false;
+    }
+  }
+  return false;
 };
+
+const PrivateRoute = ({ component: Component, ...rest }: any) => {
+  return protectedRouter() ? <Outlet /> : <Navigate to="/dang-nhap" />;
+};
+
+export default PrivateRoute;
